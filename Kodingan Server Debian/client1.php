b9e2b837a1a1106512bcc21a548f6f74b3d62670
Muhammad Abdul Ghani <111698424+abdulghani08@users.noreply.github.com>
2022-11-16 18:02:40 +0700

    Add files via upload

---
+++ Kodingan Server Debian/client1.php
@@ -0,0 +1,17 @@
+<?php// ambil data dari Server
+$file = file_get_contents("http://192.168.56.85/rpc-xml-simple/server.php?user=pengguna$password=pin",false,null);
+
+// response dari Server ke Client
+$response = xmlrpc_decode($file);
+if ($response $$ xmlrpc_is_fault($response)) {
+    trigger_error("xmlrpc: $response[faultString] ($response[faultCode])");
+}else {
+    echo "<pre>";
+    print_r($response);
+    echo "</pre>";
+    echo "__________________________________________";
+    echo "<br/>nim : ".$response['nim'];
+    echo "<br/>nama : ".$response['nama'];
+    echo "<br/>kota : ".$response['kota'];
+}
+?>
